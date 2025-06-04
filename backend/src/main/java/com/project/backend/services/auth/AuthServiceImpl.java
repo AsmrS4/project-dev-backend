6bb805f18e03ce78a9eeca20a5e6ffd2a3a0f2eb
Aymir Sunchugashev <as4n.tsu@gmail.com>
2025-06-04 18:55:25 +0700

    added db dependencies & db migration

--- backend/src/main/java/com/project/backend/services/auth/AuthServiceImpl.java
+++ backend/src/main/java/com/project/backend/services/auth/AuthServiceImpl.java
@@ -2,15 +2,41 @@ package com.project.backend.services.auth;
 
 import com.project.backend.dto.auth.LoginDto;
 import com.project.backend.dto.auth.RegisterDto;
+import com.project.backend.dto.jwt.AccessToken;
+import com.project.backend.entities.auth.User;
+import com.project.backend.repositories.UserRepository;
+import com.project.backend.utils.jwt.AccessTokenUtil;
+import com.project.backend.utils.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService{
+    private final UserRepository userRepository;
+    private final AccessTokenUtil tokenUtil;
+    private final UserMapper mapper;
     @Override
     public Object login(LoginDto loginDto) {
-        return null;
+        User user = userRepository.findUserByEmail(loginDto.getEmail());
+        if(user == null || !Objects.equals(user.getPassword(), loginDto.getPassword())) {
+            throw new RuntimeException("Неверный логин или пароль");
+        }
+        AccessToken token = tokenUtil.getAccessToken();
+        return token;
     }
 
     @Override
     public Object register(RegisterDto registerDto) {
+        if(userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new RuntimeException("Email " + registerDto.getEmail() + " занят");
+        }
+        User user = mapper.map(registerDto);
+        userRepository.save(user);
         return null;
     }
 
---
+++ backend/src/main/java/com/project/backend/utils/jwt/AccessTokenUtil.java
@@ -0,0 +1,11 @@
+package com.project.backend.utils.jwt;
+
+import com.project.backend.dto.jwt.AccessToken;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccessTokenUtil {
+    public AccessToken getAccessToken() {
+        return null;
+    }
+}
